0363267c88ea34ff664ad23e68eb1de2299956d9
bjornharink <bjorn@harink.info>
2017-11-12 18:25:21 -0800

    Bump version: 0.7.1 → 0.7.1

--- mrbles/__init__.py
+++ mrbles/__init__.py
@@ -43,7 +43,7 @@ __maintainer__ = "Bjorn Harink"
 __email__ = "bjorn@harink.info"
 # Software information
 __license__ = "MIT"
-__version__ = '__version__ = '__version__ = '0.7.1'''
+__version__ = '__version__ = '__version__ = '__version__ = '0.7.1''''
 __status__ = "Development"
 # Package settings
 __all__ = ['core', 'data']
