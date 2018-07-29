100863fad10b46fc5e7e485ed83caf8d3424d307
bjornharink <bjorn@harink.info>
2018-07-29 13:02:22 +0200

    Bumpversion v0.11.8. Updated docs.

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = '2018, Bjorn Harink'
 author = 'Bjorn Harink'
 
 # The short X.Y version
-version = '0.11.7'
+version = '0.11.8'
 # The full version, including alpha/beta/rc tags
-release = '0.11.7'
+release = '0.11.8'
 
 
 # -- General configuration ---------------------------------------------------
--- mrbles/__init__.py
+++ mrbles/__init__.py
@@ -44,7 +44,7 @@ __maintainer__ = "Bjorn Harink"
 __email__ = "bjorn@harink.info"
 # Software information
 __license__ = "MIT"
-__version__ = '0.11.7'
+__version__ = '0.11.8'
 __status__ = "Development"
 # Package settings
 __all__ = ['core', 'data']
