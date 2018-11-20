207f1daba3ad1f2f5e36a81c1c5a46e604deb642
bjornharink <bjorn@harink.info>
2018-11-20 15:26:20 -0500

    Bumpversion 0.12.0 added functionality. Fixed bugs.

--- mrbles/__init__.py
+++ mrbles/__init__.py
@@ -44,7 +44,7 @@ __maintainer__ = "Bjorn Harink"
 __email__ = "bjorn@harink.info"
 # Software information
 __license__ = "MIT"
-__version__ = '0.11.8'
+__version__ = '0.12.0'
 __status__ = "Development"
 # Package settings
 __all__ = ['core', 'data']
