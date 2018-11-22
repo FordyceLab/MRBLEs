5568d6b21336e46c5771bd8d2bda9d4de7c5285c
bjornharink <bjorn@harink.info>
2018-11-22 17:06:54 -0500

    Bumpversion 0.12.2

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = '2018, Bjorn Harink'
 author = 'Bjorn Harink'
 
 # The short X.Y version
-version = '0.12.1'
+version = '0.12.2'
 # The full version, including alpha/beta/rc tags
-release = '0.12.1'
+release = '0.12.2'
 
 
 # -- General configuration ---------------------------------------------------
